28e28fd08e1608a3d2c09c46823cbaf5f9e65621
Marius Obert <obatzer@gmail.com>
2023-05-15 23:55:25 +0200

    remove trailing part

--- dist-to-gh-pages.sh
+++ dist-to-gh-pages.sh
@@ -14,4 +14,4 @@ cp -r apps/ui5-webcomponents/dist/* docs/ui5-webcomponents
 cp -r apps/fundamental-library-styles/dist/* docs/fundamental-library-styles
 git add -f docs*
 git commit -m "Rebuild GitHub pages"
-git filter-branch -f --prune-empty --subdirectory-filter docs && git push -f origin gh-pages && git checkout -
+git filter-branch -f --prune-empty --subdirectory-filter docs && git push -f origin gh-pages
