371bbcbf10a71badcda2e693672c7c02ca62395c
Marius Obert <obatzer@gmail.com>
2023-05-15 22:35:39 +0200

    now?

---
+++ dist-to-gh-pages.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# git commit -am "Save uncommited changes (WIP)"
+git branch --delete --force gh-pages
+git checkout --orphan gh-pages
+# rm -rf docs/openui5/*
+# rm -rf docs/fiori-elements/*
+rm -rf docs/ui5-webcomponents/*
+rm -rf docs/fundamental-library-styles/*
+mkdir docs/ui5-webcomponents
+mkdir docs/fundamental-library-styles
+# cp -r apps/openui5/dist/* docs/openui5   
+# cp -r apps/fiori-elements/dist/* docs/fiori-elements   
+cp -r apps/ui5-webcomponents/dist/* docs/ui5-webcomponents   
+cp -r apps/fundamental-library-styles/dist/* docs/fundamental-library-styles
+git add -f docs*
+git commit -m "Rebuild GitHub pages"
+#git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin gh-pages && git checkout -
